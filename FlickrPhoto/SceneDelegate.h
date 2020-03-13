d2965e1a453dd1b749f09561614f6b1399243fd4
Jason <shi0jason@gmail.com>
2020-03-13 14:06:30 +0800

    Initial Commit

---
+++ FlickrPhoto/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FlickrPhoto
+//
+//  Created by cch on 2020/3/13.
+//  Copyright © 2020 cch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
