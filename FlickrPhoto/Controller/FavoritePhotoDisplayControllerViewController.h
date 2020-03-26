42a188989f2004a703a425a258c9bc4a4e4fde5d
Jason <shi0jason@gmail.com>
2020-03-26 11:29:53 +0800

    add Favorite Page

---
+++ FlickrPhoto/Controller/FavoritePhotoDisplayControllerViewController.h
@@ -0,0 +1,17 @@
+//
+//  FavoritePhotoDisplayControllerViewController.h
+//  FlickrPhoto
+//
+//  Created by cch on 2020/3/26.
+//  Copyright © 2020 cch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FavoritePhotoDisplayControllerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FlickrPhoto/Controller/FavoritePhotoDisplayControllerViewController.m
@@ -0,0 +1,22 @@
+//
+//  FavoritePhotoDisplayControllerViewController.m
+//  FlickrPhoto
+//
+//  Created by cch on 2020/3/26.
+//  Copyright © 2020 cch. All rights reserved.
+//
+
+#import "FavoritePhotoDisplayControllerViewController.h"
+
+@interface FavoritePhotoDisplayControllerViewController ()
+
+@end
+
+@implementation FavoritePhotoDisplayControllerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
